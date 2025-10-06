60d67805f809da15fbb03d2a00dfa50d7153da6d
Muhammad Aftab Liaqat <f2025-0961@bnu.edu.pk>
2025-10-06 17:26:35 +0000

    Class Assignment

---
+++ simple_calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Enter number1:\n";
+    cin >> a;
+    cout << "Enter number2:";
+    cin >> b;
+    cout << "Sum is:" << a + b << endl;
+    cout << "Product is :" << a * b << endl;
+    cout << "Difference is :" << a - b << endl;
+    cout << " Qoutient is :" << a / b << endl;
+    cout << "Modulus is :" << a % b << endl;
+
+    return 0;
+}
